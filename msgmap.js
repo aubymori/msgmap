4ab312cdca9e4c8e9b8d611c883d0446333cbadd
aubymori <aubyomori@gmail.com>
2026-01-08 14:03:59 -0600

    Usage message

--- msgmap.js
+++ msgmap.js
@@ -1,7 +1,32 @@
 const yaml = require("yaml");
 const { argv } = require("node:process");
 
+function printUsage()
+{
+    console.log(
+`USAGE: node msgmap.js [options] <dir>
+       node msgmap.js <--help|-h>
 
+Options:
+    --help, -h:
+        Displays this help message.
+
+    --out-dir, -o:
+        Output directory for the translations.
+        
+    --wide, -w:
+        Use wide character (wchar_t) strings for
+        the translations. The produced headers will
+        only work with the Microsoft Visual C++
+        compiler.`);
+}
+
+if (argv.length < 3
+|| argv[2] == "--help" || argv[2] == "-h")
+{
+    printUsage();
+    return;
+}
 
 for (let i = 2; i < argv.length; i++)
 {
